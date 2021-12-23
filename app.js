2d2528797e125c39fc1c0eb857dbc471a1f3d9a4
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-23 14:23:22 +0500

    Showing Score

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,5 +17,7 @@ form.addEventListener("submit", (e) => {
       score += 25;
     }
   });
-  console.log(score);
+
+  result.querySelector("span").textContent = `${score}%`;
+  result.classList.remove("d-none");
 });
